beebc960360c26b1785ed3cf1626d86d9a9621ef
chandeepadissanayake <chandeepadissanayake@gmail.com>
2022-03-27 19:48:29 +0530

    Auth Routes Configured.

--- routes/web/index.js
+++ routes/web/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const routerAuth = require("./auth");
+
+router.use("/auth", routerAuth);
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
