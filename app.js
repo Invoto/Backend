15896da738ee80127e04e3a192125d75ec9acf7a
chandeepadissanayake <chandeepadissanayake@gmail.com>
2022-03-27 11:04:37 +0530

    Defined database connection and model structure.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var createError = require('http-errors');
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
---
+++ config/db.js
@@ -0,0 +1,19 @@
+export default {
+    "development": {
+        "username": "root",
+        "password": null,
+        "database": "invoto",
+        "host": "127.0.0.1",
+        "dialect": "mysql",
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "invoto_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql",
+    },
+    "production": {
+        "use_env_var": 1,
+    }
+};
