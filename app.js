194da4b40421a81f8778cfc274c08b444a47ccb4
chandeepadissanayake <chandeepadissanayake@gmail.com>
2022-03-27 19:11:30 +0530

    Routes organized.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var logger = require('morgan');
 var db = require("./models");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 // Start Procedures.
 if (!process.env["INVOTO_DATABASE_SYNC"] || process.env["INVOTO_DATABASE_SYNC"] == 1) {
@@ -28,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const routerWeb = require("./web");
+const routerAPI = require("./api");
+
+router.use("/", routerWeb);
+router.use("/api", routerAPI);
 
 module.exports = router;
