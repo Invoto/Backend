dcd2e505cae4dda0413f076bf656c88f4b1e6005
chandeepadissanayake <chandeepadissanayake@gmail.com>
2022-03-29 01:02:09 +0530

    CORS whitelisted for all domains.

---
+++ config/cors.js
@@ -0,0 +1,9 @@
+
+RESPONSE_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE',
+};
+
+module.exports = {
+    RESPONSE_HEADERS,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const configCORS = require("../config/cors");
+
 const routerWeb = require("./web");
 const routerAPI = require("./api");
+
+router.use((req, res, next) => {
+    for (const [headerKey, headerValue] of Object.entries(configCORS.RESPONSE_HEADERS)) {
+        res.setHeader(headerKey, headerValue);
+    }
+
+    next();
+})
 
 router.use("/", routerWeb);
 router.use("/api", routerAPI);
